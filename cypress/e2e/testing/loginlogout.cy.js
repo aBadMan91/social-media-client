64553dcd8f3369d9f19dd0ac2ed7f3a3d4f395dd
aBadMan91 <114421846+aBadMan91@users.noreply.github.com>
2024-02-12 18:41:46 +0100

    Made some changes to the e2e test, now it correctly shows a message when login attempt fails.

--- cypress/e2e/testing/loginlogout.cy.js
+++ cypress/e2e/testing/loginlogout.cy.js
@@ -1,12 +1,13 @@
 const validEmail = "MyNameAgain24@noroff.no";
 const validPassword = "mynameagain24";
 
-const invalidEmail = "invalidEmail@email.com";
+const invalidEmail = "invalidEmail@noroff.no";
 const invalidPassword = "invalidPassword";
 
 describe("Login and logout test", () => {
   beforeEach(() => {
     cy.visit("/");
+    cy.clearLocalStorage();
     cy.wait(1000);
   });
 
@@ -18,6 +19,7 @@ describe("Login and logout test", () => {
     cy.get("#loginForm button[type='submit']").click();
     cy.wait(1000);
     cy.get("button[data-visible='loggedIn']").should("exist");
+    cy.wait(1000);
   });
 
   it("Logging out using the logout button", () => {
@@ -28,19 +30,36 @@ describe("Login and logout test", () => {
     cy.get("#loginForm button[type='submit']").click();
     cy.wait(1000);
     cy.get("button[data-visible='loggedIn']").should("exist");
+    cy.get("button[data-visible='loggedIn']").should("be.visible");
     cy.wait(1000);
     cy.get("button[data-visible='loggedIn']").click();
     cy.wait(1000);
-    cy.get("button[data-visible='loggedIn']").should("exist");
+    cy.get("button[data-visible='loggedOut']").should("exist");
+    cy.get("button[data-visible='loggedIn']").should("not.be.visible");
   });
 
-  it("Try to login with invalid credentials, but fails", () => {
+  it("Try to login with invalid credentials, but fails and shows a message", () => {
+    cy.intercept(
+      "POST",
+      "https://nf-api.onrender.com/api/v1/social/auth/login",
+    ).as("loginAttempt");
+
     cy.get("#registerForm button[data-auth='login']").click();
     cy.wait(1000);
     cy.get("#loginEmail").type(invalidEmail);
     cy.get("#loginPassword").type(invalidPassword);
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal(
+        "Either your username was not found or your password is incorrect",
+      );
+    });
     cy.get("#loginForm button[type='submit']").click();
+
+    cy.wait("@loginAttempt").then((interception) => {
+      assert.equal(interception.response.statusCode, 401);
+    });
+
     cy.wait(1000);
-    cy.window("button[data-visible='loggedIn']").should("not.exist");
+    cy.get("button[data-visible='loggedIn']").should("not.be.visible");
   });
 });
